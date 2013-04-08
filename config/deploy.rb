4d5a97962c1f7df7201958f74d14662560d5b3fc
Kronda <ephany@gmail.com>
2013-04-08 10:33:50 -0700

    depersonalized settings in config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
  # The project repo name. (one word: no spaces, dashes, or underscores)
-set :application, "test"
+set :application, "test" #FIXME
 
 # Legacy from porting from drupal, best to just leave this as "default"
 set :domains, ["default"]
 
 # Set the repository type and location to deploy from.
 set :scm, :git
-set :repository,  "git@github.com:kronda/wp_capistrano.git"
+set :repository,  "git@github.com:kronda/wp_capistrano.git" #FIXME
 # set :scm, :subversion
 # set :repository,  "https://svn.example.com/svn/#{application}/trunk/"
 # set(:scm_password) { Capistrano::CLI.password_prompt("SCM Password: ") }
@@ -18,10 +18,11 @@ set(:db_pass) { random_password }
 
 # The subdirectory within the repo containing the DocumentRoot.
 set :app_root, "webroot"
+set :local_path, "/Path/to/your/local-folder" #FIXME
 
 # Use a remote cache to speed things up
 set :deploy_via, :remote_cache
-ssh_options[:user] = 'krondaco'
+ssh_options[:user] = 'username' #FIXME
 
 # Multistage support - see config/deploy/[STAGE].rb for specific configs
 set :default_stage, "dev"
@@ -38,4 +39,4 @@ set :httpd_group, 'apache'
 
 
 #set this to where you have installed your wp site locally (http://[local_domain]/wp-admin/ should exist. )
-set :local_domain, 'test.kdev.com'
+set :local_domain, 'localhost.com' #FIXME
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,18 @@
 # Set the deployment directory on the target hosts.
-set :deploy_to, "/home/krondaco/sites/#{application}-#{stage}"
+set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
 
 # Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
-set :branch, "test"
+set :branch, "dev" #FIXME
 
 # The hostnames to deploy to.
-role :web, "kronda.com"
+role :web, "yourdomain.com" #FIXME
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "kronda.com", :primary => true
+role :db, "yourdomain.com", :primary => true #FIXME
 
 # The path to wp-cli
-set :wp, "cd #{current_path}/#{app_root} ; /home/krondaco/src/wp-cli/bin/wp"
+set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
 
 # The username on the target system, if different from your local username
-ssh_options[:user] = 'krondaco'
+ssh_options[:user] = 'username' #FIXME
